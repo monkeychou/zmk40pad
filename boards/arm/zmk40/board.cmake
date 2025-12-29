2355b7aca169d7ea0aabed8f107af48047297645
monkeychou <63698796+monkeychou@users.noreply.github.com>
2025-12-29 11:33:26 +0800

    Update board.cmake

--- boards/arm/zmk40/board.cmake
+++ boards/arm/zmk40/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-board_runner_args(uf2 "--board-id=nRF52840-pca10056-v1")
+board_runner_args(uf2 "--board-id=nRF52840-pca10056")
 
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
